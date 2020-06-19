934bc4808a3964db7e55c48528b3bc2824f4354b
chewyhewie <30371349+chewyhewie@users.noreply.github.com>
2020-06-19 19:01:49 +0800

    implement prod 2

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
-var money = 10
-var cost1 = 10
-var bought1 = 0
-var totincr = 0
+var stuff = [10,0,0];
+var cost1 = 10;
+var rcost1 = 10;
+var cost2 = 20;
+var rcost2 = 20;
 
 function click1(){
-    if (money >= cost1) {
-        money -= cost1;
-        cost1 = cost1*1.25 + 1;
-        bought1 += 1;
-        document.getElementById("currentMoney").innerHTML = money;
+    if (stuff[0] >= cost1) {
+        stuff[0] -= cost1;
+        rcost1 = rcost1*1.1 + 1;
+        cost1 = Math.round(rcost1);
+        stuff[1] += 1;
+        document.getElementById("currentMoney").innerHTML = stuff[0];
         document.getElementById("costOne").innerHTML = cost1;
-        document.getElementById("boughtOne").innerHTML = bought1;
+        document.getElementById("boughtOne").innerHTML = stuff[1];
+    }
+}
+
+function click2(){
+    if (stuff[0] >= cost2) {
+        stuff[0] -= cost2;
+        rcost2 = rcost2*1.21 + 1;
+        cost2 = Math.round(rcost2);
+        stuff[2] += 1;
+        document.getElementById("currentMoney").innerHTML = stuff[0];
+        document.getElementById("costTwo").innerHTML = cost2;
+        document.getElementById("boughtTwo").innerHTML = stuff[2];
     }
 }
 
 function loop(){
-    totincr = bought1 * 2;
-    money += totincr;
-    document.getElementById("currentMoney").innerHTML = money;
+    var i;
+    for (i = 0; i < stuff.length-1; i++){
+        stuff[i] += stuff[i+1];
+    }
+    document.getElementById("currentMoney").innerHTML = stuff[0];
+    document.getElementById("boughtOne").innerHTML = stuff[1];
 }
 
 window.setInterval(function(){
